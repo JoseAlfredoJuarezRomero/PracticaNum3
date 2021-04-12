7c61226c0c92a27641c02afde53b24b403fda445
Alfredo Juarez <Alfredo Juarez@LAPTOP-DE473M91>
2021-04-12 11:17:42 -0500

    Practica3

---
+++ ProbelmasPrac3/src/Clases/ito/poo/CuerpoCeleste.java
@@ -0,0 +1,95 @@
+package Clases.ito.poo;
+
+public class CuerpoCeleste {
+	/**
+	 * Description of the property nombre.
+	 */
+	private String nombre = "";
+
+	/**
+	 * Description of the property localizaciones.
+	 */
+	private Ubicacion localizaciones = null;
+
+	/**
+	 * Description of the property composicion.
+	 */
+	private String composicion = "";
+
+	// Start of user code (user defined attributes for CuerpoCeleste)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public CuerpoCeleste() {
+		// Start of user code constructor for CuerpoCeleste)
+		super();
+		// End of user code
+	}
+
+	/**
+	 * Description of the method desplazamiento.
+	 * @param  
+	 */
+	public void desplazamiento(int i,int j ) {
+		// Start of user code for method desplazamiento
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for CuerpoCeleste)
+
+	// End of user code
+	/**
+	 * Returns nombre.
+	 * @return nombre 
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * Sets a value to attribute nombre. 
+	 * @param newNombre 
+	 */
+	public void setNombre(String newNombre) {
+		this.nombre = newNombre;
+	}
+
+	/**
+	 * Returns localizaciones.
+	 * @return localizaciones 
+	 */
+	public Ubicacion getLocalizaciones() {
+		return this.localizaciones;
+	}
+
+	/**
+	 * Sets a value to attribute localizaciones. 
+	 * @param newLocalizaciones 
+	 */
+	public void setLocalizaciones(Ubicacion newLocalizaciones) {
+		this.localizaciones = newLocalizaciones;
+	}
+
+	/**
+	 * Returns composicion.
+	 * @return composicion 
+	 */
+	public String getComposicion() {
+		return this.composicion;
+	}
+
+	/**
+	 * Sets a value to attribute composicion. 
+	 * @param newComposicion 
+	 */
+	public void setComposicion(String newComposicion) {
+		this.composicion = newComposicion;
+	}
+
+}
+
+
+
---
+++ ProbelmasPrac3/src/Clases/ito/poo/Fruta.java
@@ -0,0 +1,185 @@
+package Clases.ito.poo;
+
+public class Fruta {
+	/**
+	 * Description of the property nombre.
+	 */
+	private String nombre = "";
+
+	/**
+	 * Description of the property extensiÃ³n.
+	 */
+	private float extensión=0F;
+
+	/**
+	 * Description of the property tiempoCosecha.
+	 */
+	private String tiempoCosecha = "";
+
+	/**
+	 * Description of the property cantCosechaxtiempo.
+	 */
+	private float cantCosechaxtiempo = 0F;
+
+	/**
+	 * Description of the property costoPromedio.
+	 */
+	private float costoPromedio = 0F;
+
+	/**
+	 * Description of the property precioVentaProm.
+	 */
+	private float precioVentaProm = 0F;
+
+	// Start of user code (user defined attributes for Empresa)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public Fruta() {
+		// Start of user code constructor for Empresa)
+		super();
+		// End of user code
+	}
+
+	/**
+	 * Description of the method agregarPeriodo.
+	 * @param  
+	 */
+	public void agregarPeriodo(String periodo,float cantEstimada ) {
+		// Start of user code for method agregarPeriodo
+		// End of user code
+	}
+
+	/**
+	 * Description of the method eliminarPeriodo.
+	 * @param  
+	 */
+	public void eliminarPeriodo( int i ) {
+		// Start of user code for method eliminarPeriodo
+		// End of user code
+	}
+
+	/**
+	 * Description of the method costoPeriodo.
+	 * @param  
+	 */
+	public void costoPeriodo(int i ) {
+		// Start of user code for method costoPeriodo
+		// End of user code
+	}
+
+	/**
+	 * Description of the method ganancia.
+	 * @param  
+	 */
+	public void ganancia(int i ) {
+		// Start of user code for method ganancia
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for Empresa)
+
+	// End of user code
+	/**
+	 * Returns nombre.
+	 * @return nombre 
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * Sets a value to attribute nombre. 
+	 * @param newNombre 
+	 */
+	public void setNombre(String newNombre) {
+		this.nombre = newNombre;
+	}
+
+	/**
+	 * Returns extensión.
+	 * @return extensión 
+	 */
+	public float getextensión(float newextensión) {
+		return this.extensión;
+	}
+
+	/**
+	 * Sets a value to attribute extensiÃ³n. 
+	 * @param newExtensiÃ³n 
+	 */
+	public void setextension(float newextensión) {
+	    this.extensión = newextensión;
+	}
+
+	/**
+	 * Returns tiempoCosecha.
+	 * @return tiempoCosecha 
+	 */
+	public String getTiempoCosecha() {
+		return this.tiempoCosecha;
+	}
+
+	/**
+	 * Sets a value to attribute tiempoCosecha. 
+	 * @param newTiempoCosecha 
+	 */
+	public void setTiempoCosecha(String newTiempoCosecha) {
+		this.tiempoCosecha = newTiempoCosecha;
+	}
+
+	/**
+	 * Returns cantCosechaxtiempo.
+	 * @return cantCosechaxtiempo 
+	 */
+	public float getCantCosechaxtiempo() {
+		return this.cantCosechaxtiempo;
+	}
+
+	/**
+	 * Sets a value to attribute cantCosechaxtiempo. 
+	 * @param newCantCosechaxtiempo 
+	 */
+	public void setCantCosechaxtiempo(float newCantCosechaxtiempo) {
+		this.cantCosechaxtiempo = newCantCosechaxtiempo;
+	}
+
+	/**
+	 * Returns costoPromedio.
+	 * @return costoPromedio 
+	 */
+	public float getCostoPromedio() {
+		return this.costoPromedio;
+	}
+
+	/**
+	 * Sets a value to attribute costoPromedio. 
+	 * @param newCostoPromedio 
+	 */
+	public void setCostoPromedio(float newCostoPromedio) {
+		this.costoPromedio = newCostoPromedio;
+	}
+
+	/**
+	 * Returns precioVentaProm.
+	 * @return precioVentaProm 
+	 */
+	public float getPrecioVentaProm() {
+		return this.precioVentaProm;
+	}
+
+	/**
+	 * Sets a value to attribute precioVentaProm. 
+	 * @param newPrecioVentaProm 
+	 */
+	public void setPrecioVentaProm(float newPrecioVentaProm) {
+		this.precioVentaProm = newPrecioVentaProm;
+	}
+
+}
+
+
+
